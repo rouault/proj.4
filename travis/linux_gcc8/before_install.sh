b1edd3c593c9f8432cbe98394b02a40958980fcc
Even Rouault <even.rouault@spatialys.com>
2020-04-22 15:10:51 +0200

    WIP [skip appveyor]

--- travis/linux_gcc8/before_install.sh
+++ travis/linux_gcc8/before_install.sh
@@ -2,14 +2,27 @@
 
 set -e
 
+# Remove pgdg.list (to avoid weird conflicts when installing i386 packages)
+sudo rm -f /etc/apt/sources.list.d/pgdg.list
+
 ./travis/before_install_apt.sh
 ./travis/before_install_pip.sh
 
-sudo apt-get install -qq \
-        lcov \
-        doxygen graphviz \
-        sqlite3 libsqlite3-dev:$ARCH \
+sudo apt-get install \
+        libsqlite3-dev:$ARCH \
         libtiff-dev:$ARCH libcurl4-openssl-dev:$ARCH
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install  g++-5
+sudo apt-get install  g++-8-multilib 
+sudo add-apt-repository -y --remove ppa:ubuntu-toolchain-r/test
+
+
+sudo apt-get install \
+        sqlite3 
+sudo apt-get install make autoconf automake lcov \
+        doxygen graphviz
 
 #scripts/cppcheck.sh
 #scripts/doxygen.sh
