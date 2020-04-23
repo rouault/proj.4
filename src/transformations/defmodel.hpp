325ad0a4aaa3e03db2d736d8b827d827441c2404
Chris Crook <ccrook@linz.govt.nz>
2020-04-23 16:22:48 +1200

    Fixes reverse step and range errors

--- src/transformations/defmodel.hpp
+++ src/transformations/defmodel.hpp
@@ -1222,7 +1222,7 @@ double Component::StepTimeFunction::evaluateAt(double dt) const {
 
 double Component::ReverseStepTimeFunction::evaluateAt(double dt) const {
     if (dt < stepEpoch.toDecimalYear())
-        return 1.0;
+        return -1.0;
     return 0.0;
 }
 
@@ -1363,16 +1363,24 @@ bool Evaluator<Grid, GridSet, EvaluatorIface>::forward(
         const auto &extent = mModel->extent();
         if (x < extent.minxRad() - EPS || x > extent.maxxRad() + EPS ||
             y < extent.minyRad() - EPS || y > extent.maxyRad() + EPS) {
-            return true;
+#ifdef DEBUG_DEFMODEL
+            iface.log("Calculation point "+toString(x)+","+toString(y)
+                +" is outside the extents of the deformation model");
+#endif                
+            return false;
         }
     }
 
-    // Check against global model temoral extent
+    // Check against global model temporal extent
     {
         const auto &timeExtent = mModel->timeExtent();
         if (t < timeExtent.first.toDecimalYear() ||
             t > timeExtent.last.toDecimalYear()) {
-            return true;
+#ifdef DEBUG_DEFMODEL
+            iface.log("Calculation epoch "+toString(t)
+                +" is not valid for the deformation model");
+#endif                
+            return false;
         }
     }
 
@@ -1697,6 +1705,9 @@ bool Evaluator<Grid, GridSet, EvaluatorIface>::forward(
                                          h_out_ignored);
         }
     }
+#ifdef DEBUG_DEFMODEL
+        iface.log("Total sum of dz: " + toString(dz));
+#endif    
     z_out += dz;
 
     return true;
